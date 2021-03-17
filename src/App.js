ebf0aa11fbe10aab6aff17a34c5be890e376c105
nourebid <nour.ebid@gmail.com>
2021-03-17 09:12:04 +0200

    enabling the search funiction and pass it to the cardlist component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, {Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox.js";
 
-const App = () => {
-    return (
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        };
+    }
+
+    searchFunc = (event) => {
+        this.setState({ searchField: event.target.value });
+    };
+
+    render() {
+
+        const fliteredRobot = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+
+        return (
         <Fragment>
-            <h1>ZTM Robo Friends App</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
+            <h1 className="text-center">ZTM Robo Friends App</h1>
+            <SearchBox searchFunc={this.searchFunc}/>
+            <CardList robots={fliteredRobot}/>
         </Fragment>
     );
+    };
 };
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { InputGroup, FormControl } from 'react-bootstrap';
+import { FaSearch } from "react-icons/fa";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
+import './SearchBox.css';
 
-const SearchBox = () => {
+const SearchBox = ({searchFunc}) => {
     return (
-        <InputGroup size="lg" className="mb-3 w-50">
-            <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
-            <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-default">Search</InputGroup.Text>
-            </InputGroup.Prepend>
+        <InputGroup className="mb-3 justify-content-center p-3">
+             <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2 p-4 border-success" onChange={searchFunc}/>
+                <span className=""><FaSearch/></span>
+            </Form>
         </InputGroup>
     )
 }
