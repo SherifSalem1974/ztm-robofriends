ebf3d0106cbbc9f661ee3f9d1d8b4f8d066e6938
nourebid <nour.ebid@gmail.com>
2021-03-21 09:57:02 +0200

    working on search input to make it responsive

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { FaSearch } from "react-icons/fa";
-import { InputGroup, FormControl, Form } from "react-bootstrap";
 import './SearchBox.css';
 
 const SearchBox = ({searchFunc}) => {
     return (
-        <InputGroup className="mb-3 justify-content-center p-3">
-             <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2 p-4 " onChange={searchFunc}/>
-                <span><FaSearch/></span>
-            </Form>
-        </InputGroup>
+        <div className="input-group mb-3 p-3 justify-content-center">
+            <input type="text" className="searchInput" placeholder="Search" onChange={searchFunc}></input>
+            <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1"><FaSearch/></span>
+            </div>
+        </div>
     )
 }
 
