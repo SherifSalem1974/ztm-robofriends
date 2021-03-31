96e1c90285e19a9eca6b3596e610a75a97791afb
nourebid <nour.ebid@gmail.com>
2021-03-31 14:00:14 +0200

    fixing styles

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ import './SearchBox.css';
 const SearchBox = ({searchFunc}) => {
     return (
         <div className="pa2 container">
-            <input type="text" className="pa3 ba br3" placeholder="Search" onChange={searchFunc}></input>
+            <input type="text" className="pa3 ba br3 ma2" placeholder="Search" onChange={searchFunc}></input>
             <div className="searchLense">
             <span className="" id="basic-addon1"><FaSearch/></span>
             </div>
