e4cf2089df6eb23de27b1f87f6ade8a834b2acb6
nourebid <nour.ebid@gmail.com>
2021-03-31 13:51:25 +0200

    working on modal and it's styles

--- src/components/CardComponenet.js
+++ src/components/CardComponenet.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './CardComponenet.css';
+import { FaRegWindowClose } from "react-icons/fa";
 
 
-const CardComponenet = ({id, name, email}) => {
+const CardComponenet = ({id, name, email, city, company, website, phone}) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return(
-        <div className="tc dib br3 pa3 ma2 grow bw2 shadow-5 card">
-            <img alt="profile_photo" src={`https://robohash.org/${id}?200x200`}/>
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
+        <>
+            <div onClick={()=> setModalIsOpen(true)} className="tc dib br3 pa3 ma2 grow bw2 shadow-5 card pointer">
+                <img alt="profile_photo" src={`https://robohash.org/${id}?200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
             </div>
-        </div>
+            <Modal 
+                isOpen={modalIsOpen} 
+                onRequestClose={()=> setModalIsOpen(false)} 
+                ariaHideApp={false} 
+                style={
+                    {
+                        content : {
+                            top: '50%',
+                            left: '50%',
+                            right: 'auto',
+                            bottom: 'auto',
+                            marginRight: '-50%',
+                            transform: 'translate(-50%, -50%)',
+                            border: 'solid 2px #9b6109',
+                            background: '#d87e43'
+                        },
+                        overlay : {
+                            background: 'rgba(0,0,0,0.65)',
+                            opacity: '0.97',
+                            cursor: 'pointer'
+                        }
+                    }
+                }
+            >
+                <FaRegWindowClose onClick={()=> setModalIsOpen(false)}></FaRegWindowClose>
+                <div className='tc'>
+                    <img alt="profile_photo" src={`https://robohash.org/${id}?200x200`}/>
+                    <h2>Name: {name}</h2>
+                    <h2>E-mail: {email}</h2>
+                    <h2>City: {city}</h2>
+                    <h2>Company: {company}</h2>
+                    <h2>Website: {website}</h2>
+                    <h2>Phone: {phone}</h2>
+                </div>
+            </Modal>
+        </>
     );
 }
 
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -7,7 +7,15 @@ const CardList = ({ robots }) => {
             {
                 robots.map((user, i) => {
                     return  (
-                        <CardComponenet key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+                        <CardComponenet 
+                        key={robots[i].id} 
+                            id={robots[i].id} name={robots[i].name} 
+                            email={robots[i].email} 
+                            city={robots[i].address.city}
+                            company={robots[i].company.name}
+                            website={robots[i].website}
+                            phone={robots[i].phone}
+                        />
                     );
                 })
             }
